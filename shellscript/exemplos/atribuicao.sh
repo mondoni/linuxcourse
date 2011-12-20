62bead9dab8d917509af32a11a7c14a49cfb44d7
Eduardo Mondoni <mondoni@linustec.com.br>
2011-12-20 14:35:55 -0200

    Adicionado pasta shellscript.

---
+++ shellscript/exemplos/atribuicao.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Variaveis
+
+# Atribuicao de texto simples.
+TEXTO=Linux
+
+# Atribuicao de texto complexo
+TEXTO2="Linux Course"
+
+# Atribuicao de numeros.
+NUMERO=8
+
+echo $TEXTO
+echo $TEXTO2
+echo $NUMERO
