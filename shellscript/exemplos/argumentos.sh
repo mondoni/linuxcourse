a5b20e8d11c8885147619a95f33a89180b5d2dfc
Eduardo Mondoni <mondoni@linustec.com.br>
2011-12-22 09:45:19 -0200

    Adicionado sript contendo exemplo de utilização de argumentos.

---
+++ shellscript/exemplos/argumentos.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Nome do script é:  $0"
+echo "O primeiro argumento é: $1"
+echo "O segundo argumento é: $2"
+echo "O número de argumentos é: $#"
+echo "Todos argumentoss juntos: $@"
