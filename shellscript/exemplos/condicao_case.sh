af40b8e84f61123131a81987b2a0a732c0d01076
Eduardo Mondoni <mondoni@linustec.com.br>
2011-12-27 14:25:54 -0200

    Adicionado script condicao_case.sh como exemplo.

---
+++ shellscript/exemplos/condicao_case.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+case $1 in
+	chaves)echo "Ninguém tem paciência comigo."
+	;;
+
+	kiko)echo "Você não vai com a minha cara?!"
+	;;
+
+	*)echo "Passe um personagem como parâmetro [chaves|kiko]."
+esac
